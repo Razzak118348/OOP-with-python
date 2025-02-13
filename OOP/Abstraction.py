28c172633c1c22b0a97beb9c2e4c1c59b79d2972
Abdur Razzak <abdurrazzak118348@gmail.com>
2025-02-13 10:44:47 +0600

    change class name

--- OOP/Abstraction.py
+++ OOP/Abstraction.py
@@ -6,7 +6,7 @@ class Account:
         self.balance=bal
         self.acc_no=acc_no
 
-    # debit method = balance - amount
+
     def debit(self,amount):
             amount=int(amount)
             if amount>self.balance:
@@ -16,7 +16,7 @@ class Account:
                 self.balance-=amount
                 print('MRP', amount ,'debited successfully')
 
-    # credit method = balance + amount
+
     def credit(self,amount):
             amount=int(amount)
             if amount<=0:
@@ -27,19 +27,19 @@ class Account:
                 print('MRP', amount ,'credited successfully')
 
 
-acc1=Account(1000,5812)
-print("your account balance is :",acc1.balance)
+user1=Account(1000,5812)
+print("your account balance is :",user1.balance)
 
 account_number= input("Enter your accoutn number:")
 account_number=int(account_number)
-if account_number==acc1.acc_no:
+if account_number==user1.acc_no:
     print('Account number matched')
     amount=input('Enter the amount you want to debit : ')
-    acc1.debit(amount)
-    print("your account balance is :",acc1.balance)
+    user1.debit(amount)
+    print("your account balance is :",user1.balance)
     amount=input('Enter the amount you want to credit : ')
-    acc1.credit(amount)
-    print("your account balance is :",acc1.balance)
+    user1.credit(amount)
+    print("your account balance is :",user1.balance)
 
 
 else:
